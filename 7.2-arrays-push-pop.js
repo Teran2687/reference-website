63cd7592877dfc07537cee4d9c618b37e9da5e13
Teran2687 <126812110+Teran2687@users.noreply.github.com>
2023-03-09 17:12:10 +0000

    adding to and removing from arrays

---
+++ 7.2-arrays-push-pop.js
@@ -0,0 +1,24 @@
+/* 
+  Warm-up
+  1. Create a global variable called favFoods and assign it to be an array strings that are your top three favorite foods.
+  2. Use console.log() to print out each element to the console individually
+*/
+
+
+
+
+// Length property examples
+
+
+
+// .push() examples
+
+
+
+// .pop() examples
+
+
+
+function problem1(){
+
+}
